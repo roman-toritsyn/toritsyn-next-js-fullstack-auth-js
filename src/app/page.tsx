14cd22472305c16de43face4dbd27cbf20293bad
Roman Toritsyn <roman.toritsyn@otakoyi.com>
2025-02-02 11:14:05 +0200

    only-coockies-logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { SignOut } from "@/components/sign-out";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import Image from "next/image";
 
 const Page = async () => {
+  const session = await auth();
+
+  if (!session) redirect("/sign-in");
+
   return (
     <>
       <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
         <p className="text-gray-600">Signed in as:</p>
-        <p className="font-medium">TODO</p>
+        <p className="font-medium">{session.user?.email}</p>
       </div>
 
       <SignOut />
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,2 +1,25 @@
 import NextAuth from "next-auth";
-export const { auth, handlers } = NextAuth({ providers: [] });
+import Github from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+
+export const { auth, handlers, signIn } = NextAuth({
+  providers: [
+    Github,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const email = "admin@admin.com";
+        const password = "1234";
+
+        if (credentials.email === email && credentials.password === password) {
+          return { email, password };
+        } else {
+          throw new Error("Invalid credentials.");
+        }
+      },
+    }),
+  ],
+});
